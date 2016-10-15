e2fac335254b0bb9edf56c5fccc3f4f3e367b2ab
Jessiane Andrade <syane_andrade@ufu.br>
2016-10-15 13:16:02 -0300

    deletado arquivos que não estava sendo usados

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from SA.Tree import *
 
 
 if __name__ == '__main__':
+    # Iniciando o diretorio raiz e o conteudo do arquivo
     snake = Tree('snake', 'rarararororoelatemehumaso')
+    # Iniciando o server no localhost na porta 8888
+    # se quiser outra porta é só editar aqui
     server = HTTPServer('localhost', 8888, snake)
     server.iniciaServer()
